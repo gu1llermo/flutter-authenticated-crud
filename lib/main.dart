cbbfd1b1906520a247f5fbbe9c6e2de20910e67e
Guillermo Nunez <guillermo6001@gmail.com>
2024-11-10 18:23:54 -0400

    instalacion formz y flutter_riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:teslo_shop/config/config.dart';
 import 'package:teslo_shop/config/router/app_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
